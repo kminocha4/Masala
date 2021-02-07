c98ae13666f9e8843ca7747ff5d9083f7fef82ab
Isha R <33170219+irajput@users.noreply.github.com>
2021-02-06 23:27:15 -0800

    m

--- main.py
+++ main.py
@@ -2,7 +2,6 @@ import discord
 import os
 import requests
 import json
-from datetime import datetime
 
 from dotenv import load_dotenv
 from discord.ext import commands,tasks
@@ -12,11 +11,13 @@ load_dotenv()
 intents = discord.Intents.default()
 intents.members = True 
 client=commands.Bot(command_prefix='.',intents=intents)
-#client = discord.Bot(prefix = '.', intents=intents)
 
-status=cycle(['Status 1','Status 2'])
+def get_quote():
+  response = requests.get("https://zenquotes.io/api/random")
+  json_data = json.loads(response.text)
+  quote = json_data[0]['q'] + " -" + json_data[0]['a']
+  return(quote)
 
-  
 @client.event
 async def on_ready():
   change_status.start()
@@ -29,7 +30,6 @@ async def ping(ctx):
 @client.command()
 async def check_In(ctx):
   print(ctx.message.author)
-  
   await ctx.author.send('Checking In!')
  
 
@@ -37,27 +37,43 @@ async def check_In(ctx):
 
 @tasks.loop(seconds=30)
 async def change_status():
-  await client.change_presence(activity=discord.Game(next(status)))
+ # try:
   for i in client.get_all_channels():
     print(i)
     if(i.name=="general"):
-      #channel=client.get_channel(i.id)
-      maya_testing = client.get_channel(807812132003512320)
-      #await channel.send("30 second check-in!")
-      msg = await maya_testing.history(limit=1).flatten()
-      timestamp = msg[0].created_at
-      current_time = datetime.now()
-      difference = current_time - timestamp
-      diff_s = difference.total_seconds()
-      if msg[0].author == client.user:
-        return
-      if (diff_s > 120):
-        await maya_testing.send('This channel is pretty dead, huh?')
+      channel=client.get_channel(i.id)
+      await channel.send("30 second check-in! :eyes:")
   for j in client.get_all_members():
-    if j.name=="fisha":
+    print(j)
+    if(j.name!="masala dosA" and j.name!="testbot" and j.name!="masala"):
       member=await client.fetch_user(j.id)
-      await member.send("60 second check-in")
+      await member.send("ISHA'S CHECK-IN :eyes:")
   #except:
-    #print("You probably need a new token!")
+  #  print("You probably need a new token! :eyes:")
 
-client.run('ODA3NzA0NzAwMzU4NzU0MzQ0.YB73ng.QxZGNYZvLXgOMiiqRt-aVSJiQJY')
+
+@client.event
+async def on_message(message):
+  if message.author == client.user:
+    return
+
+
+  if message.content.startswith('$inspire'):
+    quote = get_quote()
+    await message.channel.send(quote)
+
+@client.event
+async def on_message(message):
+  if message.author == client.user:
+    return
+
+  if message.content.startswith('friend'):
+    f1hash = message.content.find('#')
+    f1username = message.content[7:f1hash]
+    f1userdisc = message.content[f1hash + 1:]
+    for j in client.get_all_members():
+      if (j.name == f1username and j.discriminator == f1userdisc):
+        member = await client.fetch_user(j.id)
+        await member.send(message.author.name + " wants to check in with you!")
+
+client.run(os.getenv('TOKEN'))
